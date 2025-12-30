c79da378a3128af094ecfdf4a50ef420e97c1b55
Andrew Bowlds <andrew@edprealty.com>
2025-12-30 11:25:46 -0600

    Update gradle.properties to set Java home for OpenJDK 17

--- app/src/main/java/com/example/androidassistant/MainActivity.kt
+++ app/src/main/java/com/example/androidassistant/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.androidassistant
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        supportActionBar?.hide()
+        
+        findViewById<Button>(R.id.btnGetStarted).setOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
     }
 }
